5ec31b07d435ed1ac012efa1cc5d360fc06c6fdd
suhel-24 <21pa1a05g3@vishnu.edu.in>
2025-05-11 21:59:53 +0530

    "fix: backend bug fix"

--- backend/src/jobs/jobs.service.ts
+++ backend/src/jobs/jobs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 enum JobType {
   FullTime = 'FullTime',
